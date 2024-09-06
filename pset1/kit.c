f80a44f2913f24c7916fcde440af20a1e72d95ee
Noam Schuck <noam.schuck@gmail.com>
2024-09-06 17:23:22 -0400

    added designating output functionality

--- pset1/kit.c
+++ pset1/kit.c
@@ -4,9 +4,13 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    int opt, i;
+// can we assume the flag will be at the start?
 
+int report_error(char *syscall);
+
+int main(int argc, char *argv[]) {
+    int opt, i, fd_output;
+    
     // Terminate if no arguments were entered
     if(argc <= 1) {
         printf("ERROR: No arguments entered.\n\n");
@@ -22,6 +26,21 @@ int main(int argc, char *argv[]) {
         
         // Skip to next argument if the current argument 
         if(!strcmp(*arguments[i-1], "-o")) continue;
+        else fd_output = 1; // 1 is the file descripter for stdout
+
+        // If last argument was -o then set the output stream to be this file
+        if(i > 1 && !strcmp(*arguments[i-2], "-o")) {
+            fd_output = open(*arguments[i-1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+            if(errno) {
+                printf("ERROR: System call %s%s%s failed with error message \"%s (#%d)\"\n\n", "open(\"", *arguments[i-1],"\", O_WRONLY | O_CREAT | O_TRUNC, 0666)", strerror(errno), errno);
+                return -1;
+            }
+
+            continue;
+        }
+
+        
+            
     }
     
     // Terminate if the last argument was the -o flag and no output file was specified
@@ -30,3 +49,4 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 }
+
