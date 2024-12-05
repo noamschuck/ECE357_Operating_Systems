33a54d2f403c1af7ddd6c3d87a692d30a7ecde7b
Noam Schuck <noam.schuck@gmail.com>
2024-12-05 18:14:23 -0500

    generates VCPUs (processes)

--- pset6/main.c
+++ pset6/main.c
@@ -10,9 +10,12 @@
 #define NUM_SEM 3
 
 int create_sems(struct sem **sems, int rocks);
+int create_tasks(int moves);
+
+int move_count, tid;
+pid_t pid;
 
 int main(int argc, char *argv[]) {
-    int move_count, pid, tid;
     struct sem *sems;
 
     // Create the list of semaphores and initialize them
@@ -20,22 +23,65 @@ int main(int argc, char *argv[]) {
         return -1;
     }
     
-    printf("Semaphores rocks: %d %d %d\n", sems[0].count, sems[1].count, sems[2].count);
-    printf("Semaphores max_count: %d %d %d\n", sems[0].max_count, sems[1].max_count, sems[2].max_count);
-    printf("Semaphores locks: %d %d %d\n", sems[0].lock, sems[1].lock, sems[2].lock);
-    //while(wait(NULL) > 0 && errno != ECHILD);
+    // Create tasks
+    if(create_tasks(atoi(argv[2])) == -1) {
+        return -1;
+    }
+
+    switch(tid) {
+        case 7:
+            while(wait(NULL) > 0 && errno != ECHILD);
+            printf("All done!\n");
+            break;
+        default:
+            printf("VCPU %d (pid %d) has count %d left.\n", tid, pid, move_count);
+            break;
+    }
 
     return 0;
 }
 
+/* Function: create_sems()
+ * Description: Creates a list of three semaphores initialized to 3 and puts the address to the list in the contents of sems.
+ * Return Valie: Returns 0 on success and -1 on failure.
+*/
 int create_sems(struct sem **sems, int rocks) {
+
+    // Allocate space for the semaphores
     if((*sems = mmap(NULL, sizeof(struct sem)*NUM_SEM, PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0)) == MAP_FAILED) {
         perror("ERROR: mmap(NULL, sizeof(struct sem)*NUM_SEM, PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0) resulted in");
         return -1;
     }
 
+    // Initialize the semaphores
     for(int i = 0; i < NUM_SEM; i++)
         sem_init(&((*sems)[i]), rocks);
     
+    return 0;
+}
+
+/* Function: create_tasks()
+ * Description: Spawns 6 child processes and assigns them a task ID.
+ * Return Value: Returns 0 on success and -1 on failure.
+*/
+int create_tasks(int moves) {
+    for(int i = 0; i < 6; i++) {
+        if((pid = fork()) == -1) {
+            perror("ERROR: fork() resulted in");
+            return -1;
+        }
+
+        // Initializing values for the children
+        if(pid == 0) {
+            tid = i;
+            pid = getpid();
+            move_count = moves;
+            return 0;
+        }
+    }
+
+    // Initializing values for the parent
+    tid = 7;
+    pid = getpid();
     return 0;
 }
